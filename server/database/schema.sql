d350ee77212d59b741c7c2b782bdd5fa12e2c133
omega1984 <omegareborn2019@gmail.com>
2019-10-30 12:08:25 -0700

    adding schema.sql

---
+++ server/database/schema.sql
@@ -0,0 +1,11 @@
+-- CREATE DATABASE IF NOT EXISTS item_list;
+
+-- USE item_list;
+
+-- CREATE TABLE item_table (
+--   id INT PRIMARY KEY AUTO_INCREMENT,
+--   itemName text,
+--   itemId text,
+--   itemPrice decimal,
+--   itemExp datetime
+-- );
